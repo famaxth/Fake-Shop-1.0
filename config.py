07a3c9515fd706b2bc5129123b5f3db2b32b19f7
Kalir00tKali <horrorclownDM@gmail.com>
2020-12-06 18:41:40 +0300

    First commit

---
+++ config.py
@@ -0,0 +1,7 @@
+name = "text" #Название Магазина
+token = "text" #Токен бота
+admin_id = 123456789 #ID администратора
+operator = "@admin" #Логин оператора
+qiwi = "+7999999999" #Qiwi
+token_qiwi = "text" #Qiwi токен
+bitcoin = "11111111111111111" #Bitcoin
