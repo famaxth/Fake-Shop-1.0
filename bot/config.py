39d6c5b54b34e311cae0ede840cf112df957e385
Arseniy Titov <70908119+famaxth@users.noreply.github.com>
2021-08-02 20:13:56 +0300

    Add files via upload

---
+++ bot/config.py
@@ -0,0 +1,7 @@
+name = "text"  # Название Магазина
+token = "text"  # Токен бота
+admin_id = 777777777  # ID администратора
+operator = "@admin"  # Логин оператора
+qiwi = "number"  # Qiwi
+token_qiwi = "text"  # Qiwi токен
+bitcoin = "text"  # Bitcoin
