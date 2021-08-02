df21518175002f7e33f62ab6946ca4e8436ecd07
Arseniy Titov <70908119+famaxth@users.noreply.github.com>
2021-08-02 20:13:04 +0300

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-name = "text" #Название Магазина
-token = "text" #Токен бота
-admin_id = 123456789 #ID администратора
-operator = "@admin" #Логин оператора
-qiwi = "number" #Qiwi
-token_qiwi = "text" #Qiwi токен
-bitcoin = "text" #Bitcoin
